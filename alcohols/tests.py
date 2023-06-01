9b6144f4d938fbd8033dfd8795335aa5dd1f82e9
MTreczka <matreczka@gmail.com>
2023-06-01 15:25:32 +0200

    Projekt

--- alcohols/tests.py
+++ alcohols/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import pytest
+from django.test import Client
+from django.urls import reverse
+import os
+import django
 
-# Create your tests here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoProject.settings")
+django.setup()
+
+from alcohols.forms import *
+from alcohols.models import *
+from alcohols.views import *
+
+@pytest.mark.django_db
+def test_index():
+    client = Client()
+    url = reverse('index')
+    response = client.get(url)
+    assert response.status_code == 200
+
