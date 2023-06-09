f86259a60d67aca790407f752c5a27de161c8654
MTreczka <matreczka@gmail.com>
2023-06-09 16:07:30 +0200

    Projekt

--- account/forms.py
+++ account/forms.py
@@ -5,8 +5,8 @@ from django.core.exceptions import ValidationError
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    login = forms.CharField()
+    hasło = forms.CharField(widget=forms.PasswordInput)
 
 
     def clean(self):
